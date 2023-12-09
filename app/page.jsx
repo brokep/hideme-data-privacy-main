da12a25e3670a3f4d6f3f8cdbac228f47622156f
brokep <brokep@lavabit.com>
2023-12-09 17:23:09 -0600

    add speed analytics

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Cart from "./components/cart";
 import ProtectingData from "./components/protecting-data";
 import Category from "./components/category";
 import QA from "./components/QA";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <>
